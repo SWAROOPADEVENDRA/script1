b447d6d347effe940fa8a8c39705061beec6c657
swaroopa <swaroopa@gamil.com>
2024-01-29 09:33:31 +0000

    modified script3

--- script3.sh
+++ script3.sh
@@ -7,9 +7,12 @@ then
 elif [ $a -ge 13 ] && [ $a -le 19 ];
 then
         echo "teenager"
-elif [ $a -ge 20 ];
+elif [ $a -ge 20 ] && [ $a -le 60 ];
 then
         echo "adult"
+elif [ $a -ge 60 ];
+then
+	echo "Oldage"
 else
         echo "invalid input"
 fi
